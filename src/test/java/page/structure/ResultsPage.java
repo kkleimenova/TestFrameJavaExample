a333d56521f329575953f10a02aa003ce1043d15
Kateryna Kleimenova <kkaterina.poligon@gmail.com>
2017-07-15 00:13:27 +0300

    Added screenshots on fails implementation.

--- src/test/java/page/structure/ResultsPage.java
+++ src/test/java/page/structure/ResultsPage.java
@@ -1,13 +1,9 @@
-package com.test.example.pages;
+package page.structure;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-
-import java.util.concurrent.TimeUnit;
 
 public class ResultsPage{
     @FindBy(xpath = "//h3[@class='r']/a")
--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -1,25 +1,41 @@
-import com.test.example.pages.MainPage;
-import com.test.example.pages.Page;
+package tests;
+
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import io.github.bonigarcia.wdm.InternetExplorerDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import page.structure.Page;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 
 public class BaseClass {
     protected WebDriver driver;
 
+    public WebDriver getDriver() {
+        return driver;
+    }
+
     @BeforeTest
     @Parameters("browser")
     public void setup(String browser) {
+
+
         if (browser.equals("chrome")) {
             ChromeDriverManager.getInstance().setup();
             driver = new ChromeDriver();
@@ -32,6 +48,8 @@ public class BaseClass {
         }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
     }
 
     @AfterTest
@@ -40,17 +58,22 @@ public class BaseClass {
         //driver.close();
     }
 
-    @Test(description = "Verifies google search")
-    public void searchTest() throws InterruptedException {
+    @Test(description = "Verifies google opens")
+    public void appOpenTest() throws Exception {
         Page page = new Page(driver);
         page.mainPage
-                .openMainPage()
-                .assertMainPageIsLoaded()
+                .openMainPage();
+        Assert.assertNotNull(page.mainPage.assertMainPageIsLoaded());
+    }
+
+    @Test(description = "Verifies google search")
+    public void searchTest() {
+        Page page = new Page(driver);
+        page.mainPage
                 .enterSearchText()
                 .clickSubmitButton();
-        page.resultsPage
-                .assertResultsFound()
-                .redirectOnSearchedElement();
+        Assert.assertNotNull(page.resultsPage.assertResultsFound());
+        page.resultsPage.redirectOnSearchedElement();
 
 
     }
