5b7f62b998b658cec817be7af60ed8ec97f4ff5a
Kateryna Kleimenova <kkaterina.poligon@gmail.com>
2017-07-15 05:14:22 +0300

    Improvements in reporting.

--- src/test/java/tools/CustomITestListener.java
+++ src/test/java/tools/CustomITestListener.java
@@ -1,4 +1,4 @@
-package page.structure;
+package tools;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -29,10 +29,11 @@ public class CustomITestListener implements ITestListener {
         WebDriver driver = ((BaseClass) result.getInstance()).getDriver();
 
         File imageFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        String failureImageFileName = result.getMethod().getMethodName() + new SimpleDateFormat("MM-dd-yyyy_HH-ss").format(new GregorianCalendar().getTime()) + ".png";
+        String failureImageFileName = result.getMethod().getMethodName() + new SimpleDateFormat("__MM-dd-yyyy__HH-ss").format(new GregorianCalendar().getTime()) + ".png";
 
         // It will store all the screenshots in test-output/screenshots folder
         String destDir = System.getProperty("user.dir") + "/test-output/screenshots";
+        deleteFilesFromDir(destDir);
         new File(destDir).mkdirs();
         String destFile = destDir + "/" + failureImageFileName;
         try {
@@ -43,6 +44,21 @@ public class CustomITestListener implements ITestListener {
         Reporter.log("<img src=\"file:///" + destFile + "\" alt=\"\"/><br />");
 
     }
+
+    private void deleteFilesFromDir(String path) {
+        File file = new File(path);
+        File[] files = file.listFiles();
+        for (File f : files) {
+            if (f.isFile() && f.exists()) ;
+            {
+                f.delete();
+            }
+            {
+                System.out.println("ER: Can't delete files from path:" + path + " due to open or error.");
+            }
+        }
+    }
+
 
     public void onTestSkipped(ITestResult result) {
 
